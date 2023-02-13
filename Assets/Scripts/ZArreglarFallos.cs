48f94aa1e7ae86b66d6d2d2484657d530f27c00e
AntonioMVG <avilgon223@g.educaand.es>
2023-02-13 12:00:22 +0100

    Añadido archivo de arreglar fallos

---
+++ Assets/Scripts/ZArreglarFallos.cs
@@ -0,0 +1,9 @@
+// La partida se guarda, pero al cargar, la textura de las carreteras se pierde
+// así como su colocación en su correspondiente contenedor
+
+// Cuando se destruye un edificio y se cambia de edificio en el menú de abajo
+// vuelve a sonar el audio de destruir
+
+// Cuando salen personas de los edificios, salen más de la población máxima actual
+
+// Controlar el máximo de vehículos que salen, con respecto a la población actual
